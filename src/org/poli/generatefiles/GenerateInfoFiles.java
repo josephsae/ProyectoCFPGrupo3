f807079b9e766b50b82af973c9ccbacb93e95609
ACameloC <ycamelo@poligran.edu.co>
2025-03-24 11:46:20 -0500

    Agregar más nombres y lista de productos para usar (Issue #10) (#16)

--- src/org/poli/generatefiles/GenerateInfoFiles.java
+++ src/org/poli/generatefiles/GenerateInfoFiles.java
@@ -8,9 +8,10 @@ public class GenerateInfoFiles {
     private static final String DATA_DIR = "data/";
     private static final String VENDORS_FILE = DATA_DIR + "salesmen.csv"; 
     private static final String PRODUCTS_FILE = DATA_DIR + "products.csv";
-    private static final String[] FIRST_NAMES = {"Carlos", "Maria", "Luis", "Andrea", "Pedro"};
-    private static final String[] LAST_NAMES = {"Gomez", "Rodriguez", "Lopez", "Fernandez", "Martinez"};
+    private static final String[] FIRST_NAMES = {"Carlos", "Maria", "Luis", "Andrea", "Pedro", "Sofia", "Alejandra", "Jefferson", "Lorena", "Paola", "Diego", "Lucia", "Leidy", "Camila", "Juan", "Vanesa","Clara"};
+    private static final String[] LAST_NAMES = {"Gomez", "Rodriguez", "Lopez", "Fernandez", "Martinez", "Fernandez", "Martinez", "Torres", "Mendoza", "Jimenez", "Vargas", "Rios", "Coronado", "Roa", "Betancur"};
     private static final Random RANDOM = new Random();
+    private static final String[] PRODUCT_NAMES = {"Laptop", "Mouse", "Teclado", "Monitor", "Impresora", "Celular", "Tablet", "Auriculares", "Cámara", "Micrófono", "GPS", "Televisor", "Control remoto", "Camara de seguridad"};
 
     public static void main(String[] args) {
         try {
@@ -68,7 +69,7 @@ public class GenerateInfoFiles {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(PRODUCTS_FILE))) {
             for (int i = 1; i <= productCount; i++) {
                 String id = String.format("P%03d", i);
-                String name = "Product" + i;
+                String name = PRODUCT_NAMES[RANDOM.nextInt(PRODUCT_NAMES.length)] + " " + i;
                 int price = (RANDOM.nextInt(50) + 1) * 1000;
                 writer.write(id + ";" + name + ";" + price + "\n");
             }
